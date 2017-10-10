52cbe0567b8e408ffa4e138dd3e763c384b17709
151250111 <151250111@smail.nju.edu.cn>
2017-10-10 17:18:01 +0800

    修改数据库

--- service/dbconnect.js
+++ service/dbconnect.js
@@ -2,7 +2,7 @@ var mysql      = require('mysql');
 
 var config = require('../config');
 
-var pool = mysql.createPool(config.CONNECTION_SERVER);
+var pool = mysql.createPool(config.CONNECTION_PRODUCTION);
 
 exports.query = function (sql, param, callback) {
     pool.query(sql, param, callback);
