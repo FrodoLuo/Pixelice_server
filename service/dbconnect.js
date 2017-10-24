af4f0eb126714d783e63812dcf5e63d65584f9ee
FrodoLuo <359303713@qq.com>
2017-10-24 16:05:49 +0800

    迁移到sqlite

--- service/dbconnect.js
+++ service/dbconnect.js
@@ -1,15 +1,26 @@
 var mysql = require('mysql');
-var sqlite3 = require('sqlite3').verbose();
+var sqlite3 = require('sqlite3');
 
-var sqlite3Connect = new sqlite3.Database('./database/pixelice.db');
 //
 // var config = require('../config');
 //
 // var pool = mysql.createPool(config.CONNECTION_SERVER);
-
-exports.query = function query(sql, param, callback) {
-    sqlite3Connect.all(sql, param, callback);
+/**
+ *
+ * @param sql 查询sql语句, 必选
+ * @param param 可选
+ * @param callback 必选
+ */
+var query = function (sql, param, callback) {
+    callback = callback||param;
+    var sqlite3Connect = new sqlite3.Database('./database/pixelice.db');
+    sqlite3Connect.all(sql, param, function(err, result){
+        callback(err,result);
+        sqlite3Connect.close();
+    });
 };
+
+exports.query = query;
 
 exports.checkToken = function (token, callback) {
     var sql = '' +
@@ -17,3 +28,13 @@ exports.checkToken = function (token, callback) {
     var param = [token];
     sqlite3Connect.all(sql, param, callback);
 };
+
+// var sql = 'SELECT photos.photoUrl, users.nickName as author FROM photos JOIN users ON photos.userId=users.userId';
+// var k = function(err, result) {
+//     console.log(err);
+//     console.log(result);
+// };
+// sqlite3Connect.all(sql, k);
+// query('select * from users where userId=?',[59],function(err,result){
+//     console.log(result);
+// });
