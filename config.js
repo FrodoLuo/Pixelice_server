5972ee9943e551683add005702271a3f1d85c310
FrodoLuo <frodoluo@outlook.com>
2018-05-16 00:33:01 +0800

    修改邮件逻辑

--- config.js
+++ config.js
@@ -2,56 +2,57 @@
 database configs
  */
 exports.CONNECTION_LOCALHOST = {
-    host     : 'localhost',
-    user     : 'root',
-    password : 'LUOYUZHOU_1996',
-    database : 'pixelice',
+    host: 'localhost',
+    user: 'root',
+    password: 'LUOYUZHOU_1996',
+    database: 'pixelice',
     dateStrings: true
 };
 
 exports.CONNECTION_SERVER = {
-    host     : '120.24.225.58',
-    user     : 'admin',
-    password : 'LuoyuzhoU1996',
-    database : 'pixelice',
+    host: '120.24.225.58',
+    user: 'admin',
+    password: 'LuoyuzhoU1996',
+    database: 'pixelice',
     dateStrings: true
 };
 exports.CONNECTION_PRODUCTION = {
-    host     : 'localhost',
-    user     : 'root',
-    password : 'LuoyuzhoU1996',
-    database : 'pixelice',
+    host: 'localhost',
+    user: 'root',
+    password: 'LuoyuzhoU1996',
+    database: 'pixelice',
     dateStrings: true
 };
 exports.MAIL_POOL_CONFIG = {
     pool: true,
-    host: 'smtp.fstudio.top',
-    port: 465,
-    secure: true,
+    host: 'smtp-mail.outlook.com',
+    port: 587,
+    secure: false,
     auth: {
-        user: 'accountmanage@fstudio.top',
-        pass: 'LUOYUZHOU_1996'
+        user: 'frodoluo@outlook.com',
+        pass: 'lyz19960920'
     },
     tls: {
         // do not fail on invalid certs
-        rejectUnauthorized: false
+        rejectUnauthorized: false,
+        ciphers: 'SSLv3'
     }
 };
 exports.DEV = {
     CONNECTION_LOCALHOST: {
-        host     : '120.24.225.58',
-        user     : 'admin',
-        password : 'lyz19960920',
-        database : 'pixelice'
+        host: '120.24.225.58',
+        user: 'admin',
+        password: 'lyz19960920',
+        database: 'pixelice'
     },
     RESOURCE_URL: 'http://120.24.225.58/resource/'
 };
 exports.PRODUCTION = {
     CONNECTION_PRODUCTION: {
-        host     : 'localhost',
-        user     : 'root',
-        password : 'LuoyuzhoU1996',
-        database : 'pixelice'
+        host: 'localhost',
+        user: 'root',
+        password: 'LuoyuzhoU1996',
+        database: 'pixelice'
     },
     RESOURCE_URL: 'http://120.24.225.58/resource/'
 };
--- service/mailService.js
+++ service/mailService.js
@@ -3,9 +3,9 @@ var config = require('../config');
 
 const transporter = nodemailer.createTransport(config.MAIL_POOL_CONFIG);
 
-exports.sendMail = function(des, subject, content, html, callback) {
+exports.sendMail = function (des, subject, content, html, callback) {
     const sendConfig = {
-        from: 'accountmanage@fstudio.top',
+        from: 'frodoluo@outlook.com',
         to: des,
         subject: subject,
         html: html,
@@ -13,25 +13,24 @@ exports.sendMail = function(des, subject, content, html, callback) {
     };
     transporter.sendMail(sendConfig, callback)
 };
-exports.sendVerify = function(des, verifyCode, callback) {
+exports.sendVerify = function (des, verifyCode, callback) {
     console.log(des);
-   const sendConfig = {
-       from: 'accountmanage@fstudio.top',
-       to: des,
-       subject: 'Pixelice邮箱认证',
-       html: '' +
-       '<div>' +
-       '    亲爱的用户您好： <br />' +
-       '    欢迎加入Pixelice, 点击下方连接完成帐号验证.    ' +
-       '    <div>' +
-       '        <a href="http://120.24.225.58/api/auth/verify?verifyCode='+verifyCode+'">邮箱验证</a>' +
-       '    </div>' +
-       '    <div>如果您不能打开链接, 请右键复制连接到地址栏并转入.</div>' +
-       '    <div>请勿回复此邮箱</div>' +
-       '    <div>如果您并没有注册Pixelice, 请无视这封邮件</div>' +
-       '    <div>Fstudio工作室 敬上</div>' +
-       '</div>'
-   };
-   transporter.sendMail(sendConfig, callback);
+    const sendConfig = {
+        from: 'frodoluo@outlook.com',
+        to: des,
+        subject: 'Pixelice邮箱认证',
+        html: '' +
+            '<div>' +
+            '    亲爱的用户您好： <br />' +
+            '    欢迎加入Pixelice, 点击下方连接完成帐号验证.    ' +
+            '    <div>' +
+            '        <a href="http://120.24.225.58/api/auth/verify?verifyCode=' + verifyCode + '">邮箱验证</a>' +
+            '    </div>' +
+            '    <div>如果您不能打开链接, 请右键复制连接到地址栏并转入.</div>' +
+            '    <div>请勿回复此邮箱</div>' +
+            '    <div>如果您并没有注册Pixelice, 请无视这封邮件</div>' +
+            '    <div>Fstudio工作室 敬上</div>' +
+            '</div>'
+    };
+    transporter.sendMail(sendConfig, callback);
 };
-transporter
